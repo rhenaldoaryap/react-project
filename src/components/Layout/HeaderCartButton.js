90588e31f6e96ee66e958dd72d429291c0fbef79
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-05-11 11:35:04 +0700

    use context and reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,25 @@
+import { useContext } from "react";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
+  const cartContextData = useContext(CartContext);
+
+  const numberOfCartItems = cartContextData.items.reduce(
+    (previousValue, item) => {
+      return previousValue + item.amount;
+    },
+    0
+  );
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
+import { useContext } from "react";
+
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 import classes from "./MealItem.module.css";
 
-const Meal = (props) => {
+const MealItem = (props) => {
+  const cartContextData = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartContextData.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -12,10 +25,10 @@ const Meal = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
 };
 
-export default Meal;
+export default MealItem;
